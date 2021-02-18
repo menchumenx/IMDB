626e830e3ff918e4b76dc341535ac265c51854ab
Carmen Martin Garcia <menchu@MacBook-Pro-de-Carmen.local>
2021-02-18 11:18:44 +0100

    CLASE PROFESIONAL y archivo MAIN

---
+++ mainProfessional.ts
@@ -0,0 +1,16 @@
+
+import {Professional} from "./professionalClass"
+
+//Objetos de Professional
+
+let pf_1 : Professional = new Professional("Nicole kidman", 53, "femenino", 69, 180, "pelirrojo", "azul", "caucasica", false, "USA", 0, "actriz");
+let pf_2 : Professional = new Professional("Ewan McGregor", 49, "masculino", 76, 177, "castaño", "azul", "caucasica", false, "UK", 0, "actor");
+let pf_3 : Professional = new Professional("will Smith", 52, "masculino", 89, 188, "moreno", "negros", "afroamericano", false, "USA", 0, "actor");
+let pf_4 : Professional = new Professional("Julia Roberts", 53, "masculino", 67, 175, "castaño", "marron", "caucasica", false, "USA", 3, "actor");
+let pf_5 : Professional = new Professional("Peter Jacson", 59, "masculino", 90, 170, "castaño", "marron", "caucasica", false, "New Zeland", 2, "director");
+
+pf_1.printProfessional();
+pf_2.printProfessional();
+pf_3.printProfessional();
+pf_4.printProfessional();
+pf_5.printProfessional();
